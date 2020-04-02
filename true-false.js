55ff6995d3960273bb0d0ffa99f85ebfbbebb126
sanjibhero <sanjib321das@gmail.com>
2020-04-02 21:44:31 +0600

    true

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//false
+//const name = "0";
+//0
+//""
+//null
+//NaN
+//'0'
+//truthy
+let name = 12;
+console.log(name);
+
+if(name || name ==0){
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
